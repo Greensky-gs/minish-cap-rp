240c3e507ad199102fa316f16933298079ef4f30
Greensky <david.hesliere@gmail.com>
2023-02-17 18:48:45 +0100

    feat: :bento: Add emojis

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,7 +1,11 @@
+import emojis from '../data/emojis.json';
 export const utils = {
     support: 'https://discord.gg/Qt9Ns3uvYe'
 };
 
 export const util = <T extends keyof typeof utils>(key: T): typeof utils[T] => {
     return utils[key];
+};
+export const emoji = (emoji: keyof typeof emojis): string => {
+    return emojis[emoji];
 };
